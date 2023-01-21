38aaf525d8da9afa2f3e1e3fd01a4784a2bdec66
Zülfücan Karakuş <84273117+zulfucankarakus@users.noreply.github.com>
2023-01-21 20:22:29 +0300

    Initial Commit

---
+++ ZulfucanCard/ZulfucanCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZulfucanCardApp.swift
+//  ZulfucanCard
+//
+//  Created by Zülfücan Karakuş on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct ZulfucanCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
